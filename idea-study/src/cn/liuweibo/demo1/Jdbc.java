0b4d50d2492f0b84769cc777a83ef666133f6515
Weibozzz <739291780@qq.com>
2019-06-05 16:18:25 +0800

    refact:Jdbc 演示

--- idea-study/src/cn/liuweibo/demo1/Jdbc.java
+++ idea-study/src/cn/liuweibo/demo1/Jdbc.java
@@ -2,17 +2,44 @@ package cn.liuweibo.demo1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Jdbc {
-    public static void main(String[] args)throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql:///blog","root","");
-        String sql = "update user set nickName = 'test1' where id = 46";
-        Statement stmt = conn.createStatement();
-        int count = stmt.executeUpdate(sql);
-        System.out.println(count);
-        stmt.close();
-        conn.close();
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql:///blog", "root", "");
+            String sql = "update user set nickName = 'test1' where id = 46";
+            stmt = conn.createStatement();
+            int count = stmt.executeUpdate(sql);
+            System.out.println(count);
+            if (count > 0) {
+                System.out.println("更新成功！");
+            } else {
+                System.out.println("更新失败！");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
