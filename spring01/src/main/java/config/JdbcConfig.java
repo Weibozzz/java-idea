c97c020cbf454b6a5ae57c3657738cf13e34b87a
Weibozzz <739291780@qq.com>
2019-06-25 11:27:40 +0800

    feat:spring新注解

---
+++ spring01/src/main/java/config/JdbcConfig.java
@@ -0,0 +1,47 @@
+package config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class JdbcConfig {
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    /**
+     * 用于创建一个 QueryRunner 对象
+     * @param dataSource
+     * @return
+     */
+    @Bean(name = "runner")
+    public QueryRunner createQueryRunner(DataSource dataSource) {
+        return new QueryRunner(dataSource);
+    }
+
+    /**
+     * 创建数据源对象
+     *
+     * @return
+     */
+    @Bean(name = "dataSource")
+    public DataSource CreateDataSource() {
+        try {
+            ComboPooledDataSource ds = new ComboPooledDataSource();
+            ds.setDriverClass(driver);
+            ds.setJdbcUrl(url);
+            ds.setUser(username);
+            ds.setPassword(password);
+            return ds;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- spring01/src/test/java/cn/liuweibo/test/AccountServiceTest.java
+++ spring01/src/test/java/cn/liuweibo/test/AccountServiceTest.java
@@ -2,8 +2,10 @@ package cn.liuweibo.test;
 
 import cn.liuweibo.domain.Account;
 import cn.liuweibo.service.IAccountService;
+import config.SpringConfiguration;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
@@ -11,7 +13,8 @@ import java.util.List;
 public class AccountServiceTest {
     @Test
     public void testFindAll() {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         IAccountService as = ac.getBean("accountService", IAccountService.class);
         List<Account> accounts = as.findAllAccount();
         for (Account account : accounts) {
