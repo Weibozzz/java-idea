588ecdd414e054035ae3068625f4122d15480fe5
Weibozzz <739291780@qq.com>
2019-06-25 17:40:12 +0800

    refact:spring整合juint

--- spring01/src/test/java/cn/liuweibo/test/AccountServiceTest.java
+++ spring01/src/test/java/cn/liuweibo/test/AccountServiceTest.java
@@ -3,19 +3,26 @@ package cn.liuweibo.test;
 import cn.liuweibo.domain.Account;
 import cn.liuweibo.service.IAccountService;
 import config.SpringConfiguration;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class AccountServiceTest {
+    @Autowired
+    private IAccountService as = null;
     @Test
     public void testFindAll() {
 //        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-        IAccountService as = ac.getBean("accountService", IAccountService.class);
         List<Account> accounts = as.findAllAccount();
         for (Account account : accounts) {
             System.out.println(account);
